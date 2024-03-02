0865a28d8231416b07db4213ec0ae1d1bff15307
gili <gilimena95@gmail.com>
2024-03-02 15:50:16 +0200

    WIP

---
+++ App.js
@@ -0,0 +1,41 @@
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View } from "react-native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import MainScreen from "./screens/mainScreen/MainScreen";
+import { NavigationContainer } from "@react-navigation/native";
+import { Ionicons } from "@expo/vector-icons";
+
+const Tab = createBottomTabNavigator();
+
+export default function App() {
+  return (
+    <NavigationContainer>
+      <Tab.Navigator
+        screenOptions={({ route }) => ({
+          tabBarIcon: ({ focused, color, size }) => {
+            let iconName;
+            if (route.name === "Home") {
+              iconName = focused ? "home" : "home-outline";
+            } else if (route.name === "TabB") {
+              iconName = focused ? "ios-list-outline" : "ios-list";
+            }
+            return <Ionicons name={iconName} size={size} color={color} />;
+          },
+          tabBarActiveTintColor: "tomato",
+          tabBarInactiveTintColor: "gray",
+        })}
+      >
+        <Tab.Screen
+          name="Home"
+          component={MainScreen}
+          // options={{
+          //   tabBarIcon: ({ focused, color, size }) => {
+          //     return <Ionicons name="home" size={24} color="black" />;
+          //   },
+          // }}
+        />
+        <Tab.Screen name="NotHome" component={MainScreen} />
+      </Tab.Navigator>
+    </NavigationContainer>
+  );
+}
