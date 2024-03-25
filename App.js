1c6bebd2d551dfd4c81bc20a0dd95ddd94178c2b
itahTamar <tamar.verber@gmail.com>
2024-03-25 11:12:47 +0200

    update

--- App.js
+++ App.js
@@ -33,7 +33,6 @@ export default function App() {
       >
         <Tab.Screen name="Home" component={MainScreen} />
         <Tab.Screen name="Admin" component={AdminScreen} />
-        <Tab.Screen name="NotHome" component={MainScreen} />
         <Tab.Screen name="Profile" component={UserProfileScreen} />
         <Tab.Screen name="Shop" component={MainScreen} />
         <Tab.Screen name="Bag" component={MainScreen} />
