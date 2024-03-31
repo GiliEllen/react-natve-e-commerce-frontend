fef78c541e7b6f9dc7c3380319f1b2dbf1689738
Evgeny <zakman336@gmail.com>
2024-03-31 18:44:03 +0300

    update

--- screens/productScreen/ProductScreen.js
+++ screens/productScreen/ProductScreen.js
@@ -1,20 +1,60 @@
-import { View, StyleSheet } from 'react-native';
-import React from 'react';
-import ProductCard from '../../components/ProductCard';
+import React, { useEffect, useState } from 'react';
+import { View, Text, FlatList, StyleSheet, TouchableOpacity } from 'react-native';
+import { getAllProducts } from '../../api/productsApi'; 
 
-const ProductScreen = ({ navigation }) => {
+const ProductScreen = () => {
+    const [products, setProducts] = useState([]);
+    const [error, setError] = useState('');
+
+    useEffect(() => {
+        fetchProducts();
+    }, []);
+
+    const fetchProducts = async () => {
+        try {
+            const data = await getAllProducts();
+            setProducts(data);
+        } catch (error) {
+            console.error(error);
+            setError('Failed to fetch products');
+        }
+    };
+
+    const renderProduct = ({ item }) => (
+        <TouchableOpacity style={styles.productContainer}>
+            <Text style={styles.productTitle}>{item.name}</Text>
+            <Text>{item.description}</Text>
+            
+        </TouchableOpacity>
+    );
+
+    if (error) {
+        return <View style={styles.center}><Text>{error}</Text></View>;
+    }
+
     return (
-        <View style={styles.container}>
-            <ProductCard navigation={navigation} />
-        </View>
+        <FlatList
+            data={products}
+            renderItem={renderProduct}
+            keyExtractor={item => item.id.toString()}
+        />
     );
 };
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        backgroundColor: '#fff',
+    productContainer: {
+        padding: 20,
+        borderBottomWidth: 1,
+        borderBottomColor: '#cccccc',
     },
+    productTitle: {
+        fontWeight: 'bold',
+    },
+    center: {
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+    }
 });
 
 export default ProductScreen;
