a698b914c35bf34f2b871313d5cf8cd8c5e3633c
shiranlasry <128288093+shiranlasry@users.noreply.github.com>
2024-04-01 09:14:01 +0300

    addCase(updateProductAmount

--- reducers/cart/userCartSlice.js
+++ reducers/cart/userCartSlice.js
@@ -29,6 +29,28 @@ const cartItemsSlice = createSlice({
       .addCase(fetchCartItems.rejected, (state, action) => {
         state.loading = false;
         state.error = action.payload;
+      })
+      .addCase(addItemToCart.fulfilled, (state, action) => {
+        state.activeCartItems.push(action.payload.cartItem);
+      })
+      .addCase(addItemToCart.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(deleteItemFromCart.fulfilled, (state, action) => {
+        state.activeCartItems = state.activeCartItems.filter(item => item._id !== action.payload.cartItemId);
+      })
+      .addCase(deleteItemFromCart.rejected, (state, action) => {
+        state.error = action.payload;
+      })
+      .addCase(updateProductAmount.fulfilled, (state, action) => {
+        const { cartItem } = action.payload;
+        const index = state.activeCartItems.findIndex(item => item._id === cartItem._id);
+        if (index !== -1) {
+            state.activeCartItems[index] = cartItem;
+        }
+    })
+      .addCase(updateProductAmount.rejected, (state, action) => {
+        state.error = action.payload;
       });
   },
 });
