40629888caaa1a4bbebce4e1b0bd76669534e738
Lee_D <leegreat21@gmail.com>
2024-04-01 12:47:54 +0300

    cart-page-UI-additions

--- reducers/cart/userCartSlice.js
+++ reducers/cart/userCartSlice.js
@@ -1,5 +1,10 @@
 import { createSlice } from "@reduxjs/toolkit";
-import { fetchCartItems } from "./userCartApi";
+import {
+  fetchCartItems,
+  addItemToCart,
+  deleteItemFromCart,
+  updateProductAmount,
+} from "./userCartApi";
 
 const initialState = {
   activeOrder: "",
@@ -37,18 +42,22 @@ const cartItemsSlice = createSlice({
         state.error = action.payload;
       })
       .addCase(deleteItemFromCart.fulfilled, (state, action) => {
-        state.activeCartItems = state.activeCartItems.filter(item => item._id !== action.payload.cartItemId);
+        state.activeCartItems = state.activeCartItems.filter(
+          (item) => item._id !== action.payload.cartItemId
+        );
       })
       .addCase(deleteItemFromCart.rejected, (state, action) => {
         state.error = action.payload;
       })
       .addCase(updateProductAmount.fulfilled, (state, action) => {
         const { cartItem } = action.payload;
-        const index = state.activeCartItems.findIndex(item => item._id === cartItem._id);
+        const index = state.activeCartItems.findIndex(
+          (item) => item._id === cartItem._id
+        );
         if (index !== -1) {
-            state.activeCartItems[index] = cartItem;
+          state.activeCartItems[index] = cartItem;
         }
-    })
+      })
       .addCase(updateProductAmount.rejected, (state, action) => {
         state.error = action.payload;
       });
--- screens/userCart/UserCart.js
+++ screens/userCart/UserCart.js
@@ -1,5 +1,11 @@
 import React from "react";
-import { StyleSheet, Text, View, ActivityIndicator } from "react-native";
+import {
+  StyleSheet,
+  Text,
+  View,
+  ActivityIndicator,
+  Pressable,
+} from "react-native";
 import useCart from "../../app/useCart";
 
 const UserCart = () => {
@@ -21,6 +27,23 @@ const UserCart = () => {
           <Text> products: {activeCartItems.length}</Text>
         )}
       </Text>
+      <Text style={styles.coupon}>Enter your promo code</Text>
+      <Pressable style={styles.arrowBtn}>
+        <View style={styles.arrowBtnContent}>
+          <Text style={styles.arrowBtnText}> →</Text>
+        </View>
+      </Pressable>
+
+      {/* Add the price variable after total amount */}
+      <Text style={styles.total}>
+        Total amount: <Text style={styles.price}>124$</Text>
+      </Text>
+
+      <Pressable style={styles.button}>
+        <View style={styles.buttonContent}>
+          <Text style={styles.buttonText}>CHECK OUT</Text>
+        </View>
+      </Pressable>
     </View>
   );
 };
@@ -32,5 +55,73 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    position: "relative",
+  },
+
+  button: {
+    position: "absolute",
+    width: 343,
+    height: 48,
+    bottom: 20,
+  },
+  buttonContent: {
+    backgroundColor: "#DB3022",
+    flex: 1,
+    borderRadius: 25,
+    alignItems: "center",
+    justifyContent: "center",
+  },
+  buttonText: {
+    color: "white",
+  },
+  coupon: {
+    position: "absolute",
+    height: 40,
+    width: 320,
+    backgroundColor: "white",
+    color: "#9B9B9B",
+    padding: 8,
+    borderRadius: 8,
+    bottom: 140,
+
+    // Box shadow for iOS
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    // Box shadow for Android
+    elevation: 3,
+  },
+  arrowBtn: {
+    position: "absolute",
+    right: 10,
+    bottom: 140,
+    width: 40,
+    height: 40,
+    backgroundColor: "black",
+    borderRadius: 50,
+  },
+  arrowBtnContent: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  arrowBtnText: {
+    color: "white",
+    padding: 10,
+    fontSize: 15,
+    fontWeight: "bold",
+  },
+  total: {
+    position: "absolute",
+    bottom: 90,
+    left: 20,
+    color: "#9B9B9B",
+  },
+  price: {
+    color: "black",
+    fontWeight: "bold",
   },
 });
